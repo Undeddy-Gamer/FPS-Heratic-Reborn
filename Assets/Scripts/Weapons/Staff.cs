956821bce3cc3f715977e635a265f0b8d0f5c4ec
Undeddy-Gamer <54134838+Undeddy-Gamer@users.noreply.github.com>
2020-05-14 04:11:38 +1000

    projectile update

--- Assets/Scripts/Weapons/Staff.cs
+++ Assets/Scripts/Weapons/Staff.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
 
 public class Staff : MonoBehaviour
@@ -6,23 +6,26 @@ public class Staff : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public float manaShotCost = 5f;
+    public float projectileSpeed = 5;
 
     // the Higher the faster the rate of fire
-    public float rateOfFire = 15f;
+    public float rateOfFire = 10f;
 
     //the camera of the player to use for raycast hit detection
     public Camera cam;
 
-    // particle for staff weapon when fired (instead of a muzzle flash we might have magic flash)
-    public ParticleSystem weaponFire;
-    // particle for staff weapon when reload
-    public ParticleSystem weaponReload;
+    
     // a prefab for where the weapon hits
     public GameObject impactFX;
     // weapon animator
     public Animator weaponAnimator;
     // the player that will be using the Staff
     public PlayerHandler player;
+
+    [SerializeField]
+    private GameObject projectilePrefab;
+    [SerializeField]
+    public GameObject shootPosition;
 
 
     private float canFire = 0f;
@@ -50,43 +53,63 @@ public class Staff : MonoBehaviour
         { 
             //weaponFire.Play();
             Debug.Log("Zap: " + canFire);
-            RaycastHit hit;
+            
 
             weaponAnimator.Play("Shoot", -1, 0f);
             player.curMana -= manaShotCost;
 
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+
+            if (projectilePrefab != null)
             {
-                Debug.Log("Hit: " + hit.transform.name);
+                GameObject projectile = Instantiate(projectilePrefab, shootPosition.transform.position, shootPosition.transform.rotation) as GameObject;
+                projectile.GetComponent<Rigidbody>().AddForce(shootPosition.transform.forward * (projectileSpeed * 50));
+                
 
-                                
-                Target destructable = hit.transform.GetComponent<Target>();
-                Enemy enemy = hit.transform.GetComponent<Enemy>();
-
-                if (destructable != null)
-                {
-                    destructable.TakeDamage(damage);
-                }
-
-                if (enemy != null)
-                {
-                    enemy.TakeDamage(damage);
-                    //enemy.
-                }
-            
-
-                GameObject impactTemp = Instantiate(impactFX, hit.point, Quaternion.LookRotation(hit.normal));
-                Destroy(impactTemp, 2f);
-
-
+                StartCoroutine(RemoveProjectile(projectile, range / 10f));
             }
+            else
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+                {
+                    Debug.Log("Hit: " + hit.transform.name);
+
+
+                    Target destructable = hit.transform.GetComponent<Target>();
+                    Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+                    if (destructable != null)
+                    {
+                        destructable.TakeDamage(damage);
+                    }
+
+                    if (enemy != null)
+                    {
+                        enemy.TakeDamage(damage);
+                        //enemy.
+                    }
+
+
+                    GameObject impactTemp = Instantiate(impactFX, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(impactTemp, 2f);
+                }
+            }
+           
         }
 
     }
 
+    IEnumerator RemoveProjectile(GameObject projectile, float delayTime)
+    {
+        //wait for a few seconds before we destroy the arrow prefab
+        yield return new WaitForSeconds(delayTime);
+        //destroy bullet so we don't build up too many instances of the bullet
+        Destroy(projectile);
+    }
+
     public void Reload()
     {
-        weaponReload.Play();
+        //weaponReload.Play();
         player.curMana = player.maxMana;
     }
 }
