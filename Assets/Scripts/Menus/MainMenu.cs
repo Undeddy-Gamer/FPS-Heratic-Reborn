0cdce1b0c482dd83eb4a20a62877f2c95c623f6e
Undeddy-Gamer <54134838+Undeddy-Gamer@users.noreply.github.com>
2020-05-22 22:37:51 +1000

    update

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject pnlSettings;
+    public GameObject pnlMainMenu;
+    public GameObject pnlWeaponQuirkSelect;
+    
+
+    public int sceneCTF = 2;
+    public int sceneDM = 1;
+
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();
+        }
+    }
+
+    public void ToggleMenu()
+    {
+        if (GameManager.Instance.inGameMenuOpen)
+        {           
+            pnlMainMenu.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked; // lock the mouse cursor
+            Cursor.visible = false;
+            GameManager.Instance.inGameMenuOpen = false;
+            return;
+        }
+        else
+        {           
+            Cursor.lockState = CursorLockMode.None; // unlock the mouse cursor
+            Cursor.visible = true;
+            pnlMainMenu.SetActive(true);
+            GameManager.Instance.inGameMenuOpen = true;
+            return;
+        }
+    }
+
+    public void ShowSettings()
+    {
+        if (pnlSettings != null)
+        {
+            pnlSettings.SetActive(true);
+        }
+        if (pnlMainMenu != null)
+        { 
+            pnlMainMenu.SetActive(false);
+        }
+        if (pnlWeaponQuirkSelect != null)
+        {
+            pnlWeaponQuirkSelect.SetActive(false);
+        }
+    }
+
+    public void ShowMainMenu()
+    {
+        if (pnlSettings != null)
+        {
+            pnlSettings.SetActive(false);
+        }
+        if (pnlMainMenu != null)
+        {
+            pnlMainMenu.SetActive(true);
+        }
+        if (pnlWeaponQuirkSelect != null)
+        {
+            pnlWeaponQuirkSelect.SetActive(false);
+        }
+    }
+
+    public void ShowWeaponQuirkMenu()
+    {
+        if (pnlSettings != null)
+        {
+            pnlSettings.SetActive(false);
+        }
+        if (pnlMainMenu != null)
+        {
+            pnlMainMenu.SetActive(false);
+        }
+        if (pnlWeaponQuirkSelect != null)
+        {
+            pnlWeaponQuirkSelect.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        GameManager.Instance.LoadScene(0);
+    }
+
+    public void PlayCaptureTheFlag()
+    {
+        GameManager.Instance.LoadScene(sceneCTF);
+    }
+
+    public void PlayDeathMatch()
+    {
+        GameManager.Instance.LoadScene(sceneDM);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif 
+        Application.Quit();
+    }
+
+}
