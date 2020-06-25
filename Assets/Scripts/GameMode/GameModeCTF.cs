367fdb8eabf43d80c09100a984aee41687763b22
Undeddy-Gamer <54134838+Undeddy-Gamer@users.noreply.github.com>
2020-06-26 04:54:15 +1000

    ctf hud update

--- Assets/Scripts/GameMode/GameModeCTF.cs
+++ Assets/Scripts/GameMode/GameModeCTF.cs
@@ -1,11 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 /// <summary>
 /// Modified game mode for capture the flag GameMode
 /// </summary>
 public class GameModeCTF : GameMode
-{   
+{
+    [SerializeField]
+    GameObject flagCapturePanel;
+    [SerializeField]
+    TMP_Text flagCounter;
     List<Flag> flags;
+
+
+    private void Start()
+    {
+        if (flagCapturePanel != null)
+        {
+            flagCapturePanel.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (flagCapturePanel != null)
+        {
+            if (flagCounter != null)
+            {
+                flagCounter.text = "<color=red>" + teams[0].score + "</color>      <color=blue>" + teams[1].score + "</color>";
+            }
+        }
+    }
 }
